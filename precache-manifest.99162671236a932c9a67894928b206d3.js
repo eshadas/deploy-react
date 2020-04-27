a87f55f6fc9af21fee9ff1aca7810eb04d87162b
esha das <eshadas5555@gmail.com>
2020-04-27 21:08:27 +0530

    Updates

---
+++ precache-manifest.99162671236a932c9a67894928b206d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e0bb10240b7cda1a9894ed55e6aa55",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "2e7e5179fd665bc8c1b9",
+    "url": "/deploy-react/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "fe208a230d4fd56ab6ce",
+    "url": "/deploy-react/static/css/main.1d6e771e.chunk.css"
+  },
+  {
+    "revision": "2e7e5179fd665bc8c1b9",
+    "url": "/deploy-react/static/js/2.d20f20d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.d20f20d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe208a230d4fd56ab6ce",
+    "url": "/deploy-react/static/js/main.a5eff3a6.chunk.js"
+  },
+  {
+    "revision": "49774b9e183e8bf20681",
+    "url": "/deploy-react/static/js/runtime-main.c2d8d40a.js"
+  }
+]);
